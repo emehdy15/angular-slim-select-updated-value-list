f276d8d6bd9da226b05924d8efbbc840b1349e6a
emehdy15 <85126223+emehdy15@users.noreply.github.com>
2023-04-19 06:45:11 +0200

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,21 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import  { FormsModule } from "./forms/forms.module"
+import { FormsModule } from './forms/forms.module';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   template: `
-    <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+    <h1>angular-slim-select-updated-value-list
+    </h1>
+     
     <form-a></form-a>
   `,
 })
 export class App {
   name = 'Angular';
-  
-
 }
 
 bootstrapApplication(App);
